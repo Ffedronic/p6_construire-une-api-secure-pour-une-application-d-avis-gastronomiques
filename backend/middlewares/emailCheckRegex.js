72c9a72b174524d9d05f610eed538ad0f2f199e4
fedronic <fedronicfelix@gmail.com>
2021-11-12 19:35:09 +0100

    derniere modif

--- backend/middlewares/emailCheckRegex.js
+++ backend/middlewares/emailCheckRegex.js
@@ -1,6 +1,6 @@
 //création du module de vérification de la validité de l'email
 module.exports = (req, res, next) => {
-    regexEmail = /^[a-zA-Z0-9.!#$%&'*+/?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    regexEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
     const results = regexEmail.test(req.body.email); 
     if(!results){
         return res.status(401).json({message: 'adresse email invalide.'});
